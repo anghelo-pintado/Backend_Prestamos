42eb6a0a9db065877e2aaf1ed82c8044ea177bff
Anghelo Pintado <anghelopintadovalverde@gmail.com>
2025-10-15 19:37:00 -0500

    Url

--- src/main/java/com/a/prestamos/config/SecurityConfig.java
+++ src/main/java/com/a/prestamos/config/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://127.0.0.1:5500", "http://localhost:5500", ""));
+        config.setAllowedOrigins(List.of("http://127.0.0.1:5500", "http://localhost:5500", "https://frontend-presta-pe.vercel.app"));
         config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization","Content-Type"));
         config.setAllowCredentials(true);
